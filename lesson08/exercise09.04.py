9bbf575f3f9fed9e54da8e78148569eaeb880365
Christos H <63724879+BrutelCode@users.noreply.github.com>
2020-08-03 16:42:26 +0300

    Update exercise09.04.py
    Στον προηγούμενο κώδικα γραμμή '32' πραγματοποιείται αφαίρεση μεταξύ integer & string η οποία δεν μπορεί να πραγματοποιηθεί. Επίσης αφαιρώ τους ελέγχους για την επιλογή του υπολογιστή καθώς χρειαζόμαστε μόνο τον τυχαίο αριθμό(0-2). Στην εκτύπωση μετατρέπω τους αριθμούς σε string ώστε να εμφανίζονται.

--- lesson08/exercise09.04.py
+++ lesson08/exercise09.04.py
@@ -5,31 +5,23 @@ from datetime import datetime  # all 3 at the beginning
 seed(datetime.now())  # once, before randint call
 
 round = 0
-score = [0, 0] # Player, computer
+score = [0, 0]  # Player, computer
 history = []
 while True:
     round += 1
     print("Round " + str(round))
 
     # Eisodos xristi
-    player_input = int(input("Choose (0-rock, 1-scissors, 2-paper: "))
-    while player_input not in [0,1,2]:
-        print("Wrong input. Choices: rock, scissors, paper")
-        player_input = int(input("Choose (0-rock, 1-scissors, 2-paper: "))
+    player_input = int(input("Choose (0-rock, 1-scissors, 2-paper): "))
+    while player_input not in [0, 1, 2]:
+        print("Wrong input. Choices: 0-rock, 1-scissors, 2-paper")
+        player_input = int(input("Choose (0-rock, 1-scissors, 2-paper): "))
 
     # Epilogi ipologisti
-    computer_random = randrange(3)
-    if computer_random == 0:
-        computer_choice = "rock"
-    elif computer_random == 1:
-        computer_choice = "scissors"
-    else:
-        computer_choice = "paper"
-
+    computer_random = randrange(3)  # Random number from 0 to 2 (0-rock, 1-scissors, 2-paper)
 
     # Elegxos toy poios nikise
-
-    diff = player_input - computer_choice
+    diff = player_input - computer_random
 
     if diff == -1 or diff == 2:
         winner = "player"
@@ -37,7 +29,6 @@ while True:
         winner = "computer"
     else:
         winner = "no winner"
-
 
     # Diorthosi tou skor
     if winner == "player":
@@ -47,11 +38,11 @@ while True:
 
 
     # enimerwsi tou istorikou
-    history.append("Round " + str(round) + ": Player: " + player_input + ", Computer: " + computer_choice + ", Score: " + str(score[0]) + "-" + str(score[1]))
+    history.append("Round " + str(round) + ": Player: " + str(player_input) + ", Computer: " + str(computer_random) + ", Score: " + str(score[0]) + "-" + str(score[1]))
 
 
     # Ektypwsi toy nikiti kai tou skor
-    print("Computer picks: " + computer_choice)
+    print("Computer picks: " + str(computer_random))
     print("Player-Computer: " + str(score[0]) + "-" + str(score[1]))
 
     if score[0] == 3:

4679febb7b20e0cb3c7c169c74ef5d2a948a1ba8
psounis <psounis@gmail.com>
2020-08-07 00:12:38 +0300

    Update exercise1.py

--- lesson06/exercise1.py
+++ lesson06/exercise1.py
@@ -1,13 +1,19 @@
 my_list = []
-for number in range(10):
-    user_input = int(input("Give " + str(number) + ": "))
+for i in range(10):
+    user_input = int(input("Give a number: "))
+    while user_input < 10 or user_input > 20:
+        user_input = int(input("Give a number(10...20): "))
     my_list.append(user_input)
 
+print(my_list)
+
 my_tuple = tuple(my_list)
+print(my_tuple)
 
 list_squares = []
-for element in my_tuple:
-    list_squares.append(element ** 2)
+for i in range(10):
+    list_squares.append(my_list[i]**2)
 
+list_squares.sort()
 tuple_squares = tuple(list_squares)
 print(tuple_squares)

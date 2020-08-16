66f9a50c37c34d608babc231b4f2b85655cc6b35
psounis <psounis@gmail.com>
2020-08-16 21:41:03 +0300

    Delete try.except.py

--- try.except.py
+++
@@ -1,6 +0,0 @@
-try:
-    nom = int(input("Give the nominator: "))
-    denom = int(input("Give the denominator: "))
-    res = nom/denom
-except ZeroDivisionError:
-    print("Error: Division by zero")

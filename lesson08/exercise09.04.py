ae8e1340b098a75f49c5626d6dd83a871f8114ca
psounis <psounis@gmail.com>
2020-08-19 16:53:45 +0300

    Update exercise09.04.py

--- lesson08/exercise09.04.py
+++ lesson08/exercise09.04.py
@@ -12,46 +12,50 @@ while True:
     print("Round " + str(round))
 
     # Eisodos xristi
-    player_input = int(input("Choose (0-rock, 1-scissors, 2-paper: "))
-    while player_input not in [0,1,2]:
+    player_input_str = input("Choose: ")
+    while player_input_str not in ["rock", "scissors", "paper"]:
         print("Wrong input. Choices: rock, scissors, paper")
-        player_input = int(input("Choose (0-rock, 1-scissors, 2-paper: "))
+        player_input_str = input("Choose: ")
+
+    if player_input_str == "rock":
+        player_input_int = 0
+    elif player_input_str == "scissors":
+        player_input_int = 1
+    else:
+        player_input_int = 2
 
     # Epilogi ipologisti
-    computer_random = randrange(3)
-    if computer_random == 0:
-        computer_choice = "rock"
-    elif computer_random == 1:
-        computer_choice = "scissors"
+    computer_choice_int = randrange(3)
+    if computer_choice_int == 0:
+        computer_choice_str = "rock"
+    elif computer_choice_int == 1:
+        computer_choice_str = "scissors"
     else:
-        computer_choice = "paper"
-
+        computer_choice_str = "paper"
 
     # Elegxos toy poios nikise
-
-    diff = player_input - computer_choice
+    diff = player_input_int - computer_choice_int
 
     if diff == -1 or diff == 2:
         winner = "player"
-    elif diff == 1 or diff == -2:
+    elif diff == -2 or diff == 1:
         winner = "computer"
     else:
         winner = "no winner"
 
-
     # Diorthosi tou skor
+
     if winner == "player":
         score[0] += 1
     elif winner == "computer":
         score[1] += 1
 
-
     # enimerwsi tou istorikou
-    history.append("Round " + str(round) + ": Player: " + player_input + ", Computer: " + computer_choice + ", Score: " + str(score[0]) + "-" + str(score[1]))
-
+    history.append("Round " + str(round) + ": Player: " + player_input_str + ", Computer: " + computer_choice_str + ", Score: " + str(score[0]) + "-" + str(score[1]))
 
     # Ektypwsi toy nikiti kai tou skor
-    print("Computer picks: " + computer_choice)
+
+    print("Computer picks: " + computer_choice_str)
     print("Player-Computer: " + str(score[0]) + "-" + str(score[1]))
 
     if score[0] == 3:

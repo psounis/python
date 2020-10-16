c5e9e908dd863030b3a17dd50ae74d2e50e208f0
psounis <psounis@gmail.com>
2020-10-16 15:57:46 +0300

    Update exercise05.py

--- lesson11/exercise05.py
+++ lesson11/exercise05.py
@@ -9,7 +9,7 @@ def is_even(number):
 def is_prime(number):
     if number == 0 or number == 1:
         return False
-    for i in range(2, int(number / 2)):
+    for i in range(2, int(number / 2) + 1):
         if number % i == 0:
             return False
     return True
